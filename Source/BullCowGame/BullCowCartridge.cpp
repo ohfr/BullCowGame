d88c7067aebd785b2e2cbbc0b29eb78e6d5b32c2
Daniel Martin <danielmartin1205@gmail.com>
2021-03-04 19:18:28 -0500

    added logic for guessing hard coded word

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -5,12 +5,39 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
 
-    PrintLine("Welcome to Bull Cows");
-    PrintLine("Press enter to continue...");
+
+    PrintLine(TEXT("Welcome to Bull Cows"));
+    PrintLine(TEXT("Guess the %i letter word"), HiddenWord.Len());
+    PrintLine(TEXT("Press enter to continue..."));
+    InitGame(); //setting up game
+
 }
 
 void UBullCowCartridge::OnInput(const FString &Input) // When the player hits enter
 {
     ClearScreen();
-    FString HiddenWord = "cake";
+    if (!gameOver) {
+        if (lives == 0) {
+            PrintLine(TEXT("You've ran out of lives"));
+            PrintLine(TEXT("The word was: %s"), *HiddenWord);
+            gameOver = true;
+            // prompt to play again 
+        }
+
+        if (Input == HiddenWord) {
+            PrintLine(TEXT("Correct !"));
+            gameOver = true;
+        } else if (Input == ""){
+            PrintLine(TEXT("Please enter a guess!"));
+        } else {
+            PrintLine(TEXT("Incorrect, try again!"));
+            lives--;
+        }
+    }
+}
+
+void UBullCowCartridge::InitGame() {
+    gameOver = false;
+    HiddenWord = TEXT("cake");
+    lives = HiddenWord.Len();
 }
