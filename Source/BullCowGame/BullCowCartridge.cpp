289386b7e3433b4dd8d9a514876530f06d5b3ed6
Daniel Martin <danielmartin1205@gmail.com>
2021-03-04 19:28:25 -0500

    added end game logic

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -5,34 +5,32 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
 
-
-    PrintLine(TEXT("Welcome to Bull Cows"));
-    PrintLine(TEXT("Guess the %i letter word"), HiddenWord.Len());
-    PrintLine(TEXT("Press enter to continue..."));
     InitGame(); //setting up game
 
 }
 
 void UBullCowCartridge::OnInput(const FString &Input) // When the player hits enter
 {
-    ClearScreen();
-    if (!gameOver) {
+    if (!gameOver) { // game running, check player guess
         if (lives == 0) {
             PrintLine(TEXT("You've ran out of lives"));
             PrintLine(TEXT("The word was: %s"), *HiddenWord);
-            gameOver = true;
+            EndGame();
             // prompt to play again 
         }
 
         if (Input == HiddenWord) {
             PrintLine(TEXT("Correct !"));
-            gameOver = true;
+            EndGame();
         } else if (Input == ""){
             PrintLine(TEXT("Please enter a guess!"));
         } else {
             PrintLine(TEXT("Incorrect, try again!"));
             lives--;
         }
+    } else if (Input == ""){
+        ClearScreen();
+        InitGame();
     }
 }
 
@@ -40,4 +38,14 @@ void UBullCowCartridge::InitGame() {
     gameOver = false;
     HiddenWord = TEXT("cake");
     lives = HiddenWord.Len();
+
+    PrintLine(TEXT("Welcome to Bull Cows"));
+    PrintLine(TEXT("Guess the %i letter word"), HiddenWord.Len());
+    PrintLine(TEXT("Press enter to continue..."));
+}
+
+void UBullCowCartridge::EndGame() {
+    gameOver = true;
+    PrintLine(TEXT("Press Enter to play again"));
+
 }
